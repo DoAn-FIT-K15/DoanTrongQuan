52dc437ece0991e85b90a29db650c69bc63c664d
TrongQuan <142089306+DoanTrongQuan@users.noreply.github.com>
2024-08-06 17:31:53 +0700

    update file docker file

--- cinema-backend/src/main/java/org/example/project_cinemas_java/ProjectCinemasJavaApplication.java
+++ cinema-backend/src/main/java/org/example/project_cinemas_java/ProjectCinemasJavaApplication.java
@@ -1,15 +1,19 @@
 package org.example.project_cinemas_java;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+
+
 public class ProjectCinemasJavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectCinemasJavaApplication.class, args);
+
     }
 
 }
